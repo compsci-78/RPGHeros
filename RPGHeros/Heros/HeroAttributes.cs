8bad53f25ed8d79b2e0abccb2f196804d74c7397
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-02-09 14:11:58 +0100

    Added +operator overloading to the HeroAttributes class.

--- RPGHeros/Heros/HeroAttributes.cs
+++ RPGHeros/Heros/HeroAttributes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Security.Principal;
 using System.Text;
@@ -13,9 +14,27 @@ namespace RPGHeros.Heros
         public int Dexterity { get; set; }
         public int Intelligence { get; set; }
 
-        public int Sum()
+        public override bool Equals(object? obj)
         {
-            return 0;
+            return obj is HeroAttributes attributes &&
+                   Strength == attributes.Strength &&
+                   Dexterity == attributes.Dexterity &&
+                   Intelligence == attributes.Intelligence;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Strength, Dexterity, Intelligence);
+        }
+
+        public static HeroAttributes operator +(HeroAttributes leftSide, HeroAttributes rightSide)
+        {
+            return new HeroAttributes()
+            {
+                Strength = leftSide.Strength + rightSide.Strength,
+                Dexterity = leftSide.Dexterity = rightSide.Dexterity,
+                Intelligence = leftSide.Dexterity + rightSide.Intelligence
+            };
         }
     }
 }
