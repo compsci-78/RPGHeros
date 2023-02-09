095660fba91164a1ac315b2fa0cf6b74282c166d
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-02-09 08:44:44 +0100

    Added custom exception classes.

---
+++ RPGHeros/Exceptions/InvalidArmorTypeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeros.Exceptions
+{
+    public class InvalidArmorTypeException:Exception
+    {
+        public InvalidArmorTypeException() { }
+
+        public InvalidArmorTypeException(string message) : base(message) { }
+
+        public override string Message => "Invalid armor type.";
+
+    }
+}
---
+++ RPGHeros/Exceptions/InvalidWeaponTypeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeros.Exceptions
+{
+    public class InvalidWeaponTypeException:Exception
+    {
+        public InvalidWeaponTypeException() { }
+        public InvalidWeaponTypeException (string message) : base(message) { }
+
+        public override string Message => "Invalid weapon type."; 
+
+    }
+}
