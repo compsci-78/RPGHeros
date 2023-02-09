7894122811fbee4cd716b2f4fbd513d1a735db0e
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-02-09 13:42:00 +0100

    Added initialization values in Ranger constructor.

--- RPGHeros/Heros/Ranger.cs
+++ RPGHeros/Heros/Ranger.cs
@@ -1,4 +1,5 @@
-﻿using RPGHeros.Items;
+﻿using RPGHeros.Enums;
+using RPGHeros.Items;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,20 @@ namespace RPGHeros.Heros
     {
         public Ranger(string name) : base(name)
         {
+            Level = 1;
+            LevelAttributes = new HeroAttributes() { Strength = 1, Dexterity = 7, Intelligence = 1 };
+            Equipment = new Dictionary<Enums.Slot, Item>(4);
+            Equipment.Add(Slot.Weapon, null);
+            Equipment.Add(Slot.Head, null);
+            Equipment.Add(Slot.Body, null);
+            Equipment.Add(Slot.Legs, null);
+
+            ValidWeaponTypes = new List<WeaponType>();
+            ValidWeaponTypes.Add(WeaponType.Bows);            
+
+            ValidArmorTypes = new List<ArmorType>();
+            ValidArmorTypes.Add(ArmorType.Leather);
+            ValidArmorTypes.Add(ArmorType.Mail);
         }
 
         public override int Damage()
